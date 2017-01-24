3f0e4e04f10d125a12f1bc3d329af49221d4dd27
dsweetser <daniel.sweetser@gmail.com>
2017-01-24 14:51:55 -0500

    style game-logic
    -fixed grunt nag errors on assets/scripts/game-logic.js

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -1,19 +1,19 @@
 'use strict';
 
-let cells = ["","","","","","","","",""];
+let cells = ['', '', '', '', '', '', '', '', ''];
 
 const triggerEndGame = function (board) {
-  if ((board[0] && board[1] && board[2] === "x") || (board[3] && board[4] && board[5] === "x") ||
-    (board[6] && board[7] && board[8] === "x") || (board[0] && board[4] && board[8] === "x") ||
-    (board[2] && board[4] && board[6] === "x")) {
-      console.log("X Wins!");
-    } else if ((board[0] && board[1] && board[2] === "o") || (board[3] && board[4] && board[5] === "o") ||
-    (board[6] && board[7] && board[8] === "o") || (board[0] && board[4] && board[8] === "o") ||
-    (board[2] && board[4] && board[6] === "o")) {
-        console.log("O Wins!");
-      }
+  if ((board[0] && board[1] && board[2] === 'x') || (board[3] && board[4] && board[5] === 'x') ||
+      (board[6] && board[7] && board[8] === 'x') || (board[0] && board[4] && board[8] === 'x') ||
+      (board[2] && board[4] && board[6] === 'x')) {
+    console.log('X Wins!');
+  } else if ((board[0] && board[1] && board[2] === 'o') || (board[3] && board[4] &&
+      board[5] === 'o') || (board[6] && board[7] && board[8] === 'o') || (board[0] &&
+        board[4] && board[8] === 'o') || (board[2] && board[4] && board[6] === 'o')) {
+    console.log('O Wins!');
+  }
 };
 
 triggerEndGame(cells);
 
-// sample testing arrays ["x","","","x","","","x","",""], ["x","","","","x","","","","x"]
+// sample testing arrays ['x','','','x','','','x','',''], ['x','','','','x','','','','x']
