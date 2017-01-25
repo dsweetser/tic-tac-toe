d7ae6a95152d24c172e6f5262ae6b463fed5c706
dsweetser <daniel.sweetser@gmail.com>
2017-01-25 15:16:08 -0500

    add event handlers
    -add event handlers to click events, still only registers first click

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -12,7 +12,7 @@ const triggerEndGame = function (board) {
       ((board[2] === 'x') && (board[5] === 'x') && (board[8] === 'x')) ||
       ((board[0] === 'x') && (board[4] === 'x') && (board[8] === 'x')) ||
       ((board[2] === 'x') && (board[4] === 'x') && (board[6] === 'x'))) {
-    return ('X Wins!');
+    console.log('X Wins!');
   } else if
       (((board[0] === 'o') && (board[1] === 'o') && (board[2] === 'o')) ||
       ((board[3] === 'o') && (board[4] === 'o') && (board[5] === 'o')) ||
@@ -22,7 +22,7 @@ const triggerEndGame = function (board) {
       ((board[2] === 'o') && (board[5] === 'o') && (board[8] === 'o')) ||
       ((board[0] === 'o') && (board[4] === 'o') && (board[8] === 'o')) ||
       ((board[2] === 'o') && (board[4] === 'o') && (board[6] === 'o'))) {
-    return ('O Wins!');
+    console.log('O Wins!');
   } else if (turnCounter > 8) {
     return ('Nobody Wins!');
   }
@@ -44,15 +44,6 @@ const turnOrder = function (num) {
 
   turnCounter++;
 };
-
-// this function tests the above logic, will need to be reimplemented elsewhere
-const testLogic = function (square) {
-  turnOrder(square);
-  console.log(board);
-  return triggerEndGame(board);
-};
-
-testLogic();
 
 module.exports = {
   triggerEndGame,
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,28 +5,44 @@ const config = require('./config');
 const gameLogic = require('./game-logic');
 
 const createBoard = function () {
-  // gameLogic.turnCounter = 0;
-
+  $('#board').html('<div class="container board" id="board"></div>');
   for (let i = 0; i < 9; i++) {
-    $('#board').append('<div class="col-xs-4" id="square' + i + '"></div>');
+    $('#board').append('<div class="col-xs-4" id="' + i + '"></div>');
     if (gameLogic.board[i] === 'x') {
-      $('#square' + i).text('X');
+      $('#' + i).text('X');
     } else if (gameLogic.board[i] === 'o') {
-      $('#square' + i).text('O');
+      $('#' + i).text('O');
     }
   }
 };
 
-createBoard();
+// this function tests the above logic, will need to be reimplemented elsewhere
+const testLogic = function (event) {
+  event.preventDefault();
+  gameLogic.turnOrder(parseInt(event.target.id));
+  console.log(gameLogic.board);
+  return gameLogic.triggerEndGame(gameLogic.board);
+};
+
+const addHandlers = () => {
+  $('#0').on('click', testLogic);
+  $('#1').on('click', testLogic);
+  $('#2').on('click', testLogic);
+  $('#3').on('click', testLogic);
+  $('#4').on('click', testLogic);
+  $('#5').on('click', testLogic);
+  $('#6').on('click', testLogic);
+  $('#7').on('click', testLogic);
+  $('#8').on('click', testLogic);
+};
 
 $(() => {
   setAPIOrigin(location, config);
 
-  // createBoard();
-
   //on click, turns div to X or O and checks triggerEndGame
   //  $().on('click')
 
+  createBoard();
 });
 
 // use require with a reference to bundle the file and use it in this file
@@ -43,4 +59,5 @@ const authEvents = require('./auth/events.js');
 
 $(() => {
   authEvents.addHandlers();
+  addHandlers();
 });
