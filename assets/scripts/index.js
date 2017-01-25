5642e5dadfa0b1c095648ec2a05efdd5b6f4b54d
dsweetser <daniel.sweetser@gmail.com>
2017-01-24 20:45:16 -0500

    created turnOrder function
    -created turn order function that will mark whether it is X or O's turn
    and increment to the other afterwards

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,8 +3,27 @@
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
 
+let turnCounter = 0;
+let board = ['', '', '', '', '', '', '', '', ''];
+
+//determines if click is X or O
+const turnOrder = function (num) {
+  if (turnCounter % 2 === 0) {
+    board[num] = 'x';
+  } else {
+    board[num] = 'o';
+  }
+
+  turnCounter++;
+};
+
 $(() => {
   setAPIOrigin(location, config);
+  turnOrder();
+
+  //on click, turns div to X or O and checks triggerEndGame
+  //  $().on('click')
+
 });
 
 // use require with a reference to bundle the file and use it in this file
