43b4640bff960e922b99e05e62c459377ce7b1c3
dsweetser <daniel.sweetser@gmail.com>
2017-01-25 12:18:07 -0500

    start createBoard
    -started the create board function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,19 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
+const gameLogic = require('game-logic');
+
+const createBoard = function () {
+  gameLogic.board = ['', '', '', '', '', '', '', '', ''];
+  gameLogic.turnCounter = 0;
+  for (let i = 0; i < 9; i++) {
+    $('#board').append('<div class="square"></div>');
+  }
+};
 
 $(() => {
   setAPIOrigin(location, config);
+  createBoard();
 
   //on click, turns div to X or O and checks triggerEndGame
   //  $().on('click')
